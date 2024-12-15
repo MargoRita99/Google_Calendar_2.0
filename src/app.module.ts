62f8bc22e1f8f1799b7c1061aa16e9e79c36ef97
Margarita Makarova <makarova.mar93@gmail.com>
2024-12-15 17:07:18 +0300

    Updated app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,11 @@ import { UserModule } from './user/user.module';
   imports: [
     TypeOrmModule.forRoot({ 
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'your_username',
-      password: 'your_password',
-      database: 'your_database',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true, 
     }),
